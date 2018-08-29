e3280b6de483e4a03b468bae6cc437c7beff075e
cynsdaemon <cynsdaemon@users.noreply.github.com>
2018-08-28 16:21:58 -0800

    fix allEnemies error

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,22 @@
-// Enemies our player must avoid
-var Enemy = function() {
+// Enemy class
+let Enemy = function() {
     // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    this.x = 250 //
+    this.y = 300 //
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+// Enemy methods:
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+// TODO: update position:
+    // if enemy is not passed boundary
+        // move forward
+        // increment x by speed * dt
+    // else
+        // reset pos to start
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,19 +24,46 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player class
+let Player = function() {
+    // this.x = 250;
+    // this.y = 300;
+    this.sprite = 'images/char-cat-girl.png';
+};
 
+// Player methods:
+Player.prototype.update = function(dt){
+    // TODO: update position, update()
+    // check collison here
+        // did player x and y collide with enemy?
+        // did player reach the water?
+            // check for victory function
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+};
 
+// Player.protoytype.render = function() {
+//     // TODO: rendering the player image
+//     // TODO: draw sprite on current x and y coord position
+//     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+// };
 
+Player.prototype.handleInput = function() {
+    // Event Handlers:
+    // handleInput()
+        // event listener for keyboard updates player position
+};
+// init player/enemy objects
+let player = [];
+let allEnemies = [new Enemy()];
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// TODO:
+    // for each enemy created, push new Enemy object into above array
+// TODO: Reset game
+    // if game is won/lost
+    // display modal
+
+/*** You don't need to modify this ***/
+// This listens for keyboard presses for player character
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
--- scripts/engine.js
+++ scripts/engine.js
@@ -9,16 +9,16 @@
  * drawn but that is not the case. What's really happening is the entire "scene"
  * is being drawn over and over, presenting the illusion of animation.
  *
- * This engine makes the canvas' context (ctx) object globally available to make 
+ * This engine makes the canvas' context (ctx) object globally available to make
  * writing app.js a little simpler to work with.
  */
 
-var Engine = (function(global) {
+let Engine = (function(global) {
     /* Predefine the variables we'll be using within this scope,
      * create the canvas element, grab the 2D context for that canvas
      * set the canvas elements height/width and add it to the DOM.
      */
-    var doc = global.document,
+    let doc = global.document,
         win = global.window,
         canvas = doc.createElement('canvas'),
         ctx = canvas.getContext('2d'),
@@ -38,7 +38,7 @@ var Engine = (function(global) {
          * would be the same for everyone (regardless of how fast their
          * computer is) - hurray time!
          */
-        var now = Date.now(),
+        let now = Date.now(),
             dt = (now - lastTime) / 1000.0;
 
         /* Call our update/render functions, pass along the time delta to
@@ -79,7 +79,7 @@ var Engine = (function(global) {
      */
     function update(dt) {
         updateEntities(dt);
-        // checkCollisions();
+        //checkCollisions();
     }
 
     /* This is called by the update function and loops through all of the
@@ -93,7 +93,7 @@ var Engine = (function(global) {
         allEnemies.forEach(function(enemy) {
             enemy.update(dt);
         });
-        player.update();
+        //player.update();
     }
 
     /* This function initially draws the "game level", it will then call
@@ -106,7 +106,7 @@ var Engine = (function(global) {
         /* This array holds the relative URL to the image used
          * for that particular row of the game level.
          */
-        var rowImages = [
+        let rowImages = [
                 'images/water-block.png',   // Top row is water
                 'images/stone-block.png',   // Row 1 of 3 of stone
                 'images/stone-block.png',   // Row 2 of 3 of stone
@@ -117,7 +117,7 @@ var Engine = (function(global) {
             numRows = 6,
             numCols = 5,
             row, col;
-        
+
         // Before drawing, clear existing canvas
         ctx.clearRect(0,0,canvas.width,canvas.height)
 
